2e0ba1539664fe8aed4b5c5c37df5b7f0638b769
Ranjith <ranjithpks2002@gamil.com>
2025-11-27 11:10:09 +0530

    first commit

---
+++ client/src/lib/utils.js
@@ -0,0 +1,10 @@
+export function formatMessageTime(date){
+
+    return new Date(date).toLocaleTimeString('en-us',{
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+
+    })
+
+}
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import assets from "../assets/assets";
+
+const LoginPage = () => {
+  const [currState, setCurrState] = useState("Sing up");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted,setIsDataSubmitted]=useState("false")
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center
+    justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl"
+    >
+      {/* left */}
+      <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
+      {/* right */}
+      <form
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col 
+     gap-6 rounded-lg shadow-lg"
+      >
+        <h2 className="font-medium text-2xl flex justify-between items">
+          {currState}
+          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+        </h2>
+          {currState === "Sign up" && !isDataSubmitted && (
+                <input  onChange={(e)=>setFullName(e.target.value)} value={fullName}
+                 type="text"
+          className="p-2 border border-gray-500 rounded-md focus:outline-none"
+          placeholder="Full Name"
+          required />
+          )}
+
+          {!isDataSubmitted && (
+            <>
+            <input onChange={(e)=>setEmail(e.target.value)} value={email}
+             type="email" placeholder="Email Address" required className="
+            p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+             />
+                <input onChange={(e)=>setPassword(e.target.value)} value={password}
+             type="password" placeholder="Password" required className="
+            p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+             />
+
+            </>
+          )}
+    
+    {
+      currState === "Sign up " && isDataSubmitted && (
+        <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+        rows={4} className="p-2 border border-gray-500 rounded-md
+        focus:outline-none focus:ring-2 focus:ring-indigo-500" 
+        placeholder="Provide a short bio...." required></textarea>
+      )
+    }
+    <button type="submit" className="py-3 bg-gradient-to-r form-purple-400 to-violet-600
+    text-white rounded-md cursor-pointer">
+      {currState === "Sign up" ? "Create Account":"Login Now"}
+    </button>
+
+    <div >
+      <input type="chechbox" />
+      <p>Agree to the terms of use & privay policy</p>
+
+    </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
